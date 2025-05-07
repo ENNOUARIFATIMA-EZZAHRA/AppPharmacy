f0c9530cb2acb53ffbf072666f261f0d1b706c4a
ENNOUARI FATIMA-EZZAHRA <fatimaezzahraennouari@gmail.com>
2025-05-07 17:53:15 +0100

    ajouter la fonction de ajouter et afficher

--- pharmacy/src/main/java/com/devtiro/pharmacy/controllers/ProductController.java
+++ pharmacy/src/main/java/com/devtiro/pharmacy/controllers/ProductController.java
@@ -1,13 +1,34 @@
 package com.devtiro.pharmacy.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.devtiro.pharmacy.model.ProductModel;
+import com.devtiro.pharmacy.service.ProductService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/api/product")
 public class ProductController {
-    @GetMapping("/hello")//pour recuperer data
 
-    public String hello(){
-        return "hello world";
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
     }
+
+    // Create Product
+    @PostMapping
+    public ResponseEntity<ProductModel> saveProduct(@RequestBody ProductModel product) {
+        ProductModel savedProduct = productService.saveProduct(product);
+        return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+    }
+
+    // Get All Products
+    @GetMapping
+    public List<ProductModel> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
 }
